8950ade8795c806b4eecaa1a4139403a0cfc374c
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-04 18:48:12 +1100

    final

--- review-project/src/Components/Review.js
+++ review-project/src/Components/Review.js
@@ -4,25 +4,27 @@ import {FaChevronLeft, FaChevronRight, FaPeopleArrows, FaQuoteRight} from 'react
 
 const Review = () => {
 
-    const[index, setIndex] = useState(1);
+    const[index, setIndex] = useState(0);
    const {name, job, image, text } = data[index];
    //function to check the index
-
-   const checkIndex = (index)=>{
-       if(index> data.length-1){
-          return 0;
-       }
-       if(index < 0){
-           return data.length-1;
-       }
-       return index;
-   }
-//    for random
-const random = ()=>{
-   let number = Math.floor( Math.random() * data.length);
-   return number;
-
+const checkFn =(index)=>{
+    if(index>data.length-1){
+        return 0;
+    }
+    if(index<0){
+        return data.length-1;
+    }
+    return index;
 }
+
+//generate random quotes
+
+const randomBtn = ()=>{
+    let newIndex = Math.floor(Math.random()*data.length);
+    console.log(newIndex+1)
+
+return newIndex}
+
     return <>
     <main className="container"> 
 <article>
@@ -41,19 +43,15 @@ const random = ()=>{
 </section>
 
 <div className="btn-container">
-    <button className="prev-btn" onClick={()=>{setIndex( (index)=>{
-let newIndex = index +1;
-return checkIndex( newIndex);
-    } )}}><FaChevronLeft/> </button>
-      <button className="prev-btn" onClick={()=>{setIndex((index)=>{
-let newIndex = index-1;
-return checkIndex(newIndex);
-      })}}><FaChevronRight/> </button>
-</div>
-<button className="fnl-btn" onClick={()=>setIndex(random())}> Surprise Me</button>
-
+  <button className='prev-btn' onClick={
+      ()=>setIndex(checkFn(index+1))
+  }> <FaChevronLeft/> </button>
+  <button className='prev-btn2'onClick={()=>setIndex(checkFn(index-1))}> <FaChevronRight/></button>
  
-
+</div>
+<button className="fnl-btn" onClick={
+  ()=>setIndex(randomBtn)
+}> Surprise Me</button>
 
 </article>
 
